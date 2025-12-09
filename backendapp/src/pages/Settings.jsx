7704992bb0c822470dbad255b1d337c4c39717da
shahdMohammad <shahdmohammadx@gmail.com>
2025-12-09 21:28:35 +0200

    feat: add first section in settings page

--- backendapp/src/pages/Settings.jsx
+++ backendapp/src/pages/Settings.jsx
@@ -4,6 +4,7 @@ import Navbar from '../component/Navbar';
 import SectionTitle from '../component/SectionTitle';
 import Titlewinput from '../component/Titlewinput';
 import profileimg from "../assets/profile.svg"
+import Button from '../component/Button';
 const Settings = () => {
     return ( 
         <>
@@ -11,21 +12,47 @@ const Settings = () => {
           <div className='nabarwithmain'>
         <Navbar/>
         <div className='mainBar'>
-             <div className='titlewsearch'>
+             <div className='titlewsearch width90'>
             <SectionTitle Sectiontitle="Settings"/>
     
             </div>
 
             <div className='profilesettingdiv'>
+                                                    <div className='imgcontwidth'>
+
                   <SectionTitle textsize="24px" 
                 Sectiontitle="Profile settings"/>
+</div>
+                                    <div className='imgcontwidth'>
+
                 <img src={profileimg} alt="profile image" className='profileicon'/>
+                </div>
                 <div className='inputs2sections'>
                     <div className='section1inputs'>
                         <Titlewinput inputtile="Name" 
                         placetext="Shahd Mohammad"
                         />
+   <Titlewinput inputtile="Country" 
+                        placetext="Egypt"
+                        />   <Titlewinput inputtile="Role" 
+                        placetext="UX/UI Designer"
+                        />
+                    </div>
+                     <div className='section1inputs'>
+                        <Titlewinput inputtile="Linkedin" 
+                        placetext="Shahd Mohammad"
+                        />
+   <Titlewinput inputtile="Behance" 
+                        placetext="Shahd Mohammad"
+                        />   <Titlewinput inputtile="Bio" 
+                        
+                        />
+                    </div>
+                    <div className='imgcontwidth2'>
 
+                    <Button buttontext="Save"
+                    buttonwidth="210px
+"/>
                     </div>
 
                 </div>
